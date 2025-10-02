077caa8f1fe1cbaf52121ed70353b7297b1d46b7
Not Nahid <nahidul.live@gmail.com>
2025-10-02 23:26:30 +0600

    run.sh

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ echo "--- Installing system packages ---"
 sudo apt-get update
 sudo apt-get install -y python3-pip python3-tk libappindicator3-1
 
-echo "--- Installing Python packages ---"
-pip3 install -r requirements.txt
+echo "--- Installing Python packages (bypassing system protection) ---"
+pip3 install -r requirements.txt --break-system-packages
 
 echo "--- Installation complete! ---"
 echo "--- You can now run the application. ---"

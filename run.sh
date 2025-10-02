a774da5e5779efadb560bc8eec0bb714d9b91b9f
Not Nahid <nahidul.live@gmail.com>
2025-10-02 23:17:33 +0600

    run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-   2     echo "--- Installing system packages and Python dependencies ---"
-   3     sudo apt-get update && sudo apt-get install -y python3-pip python3-tk libappindicator3-1
-   4     pip3 install -r requirements.txt
-   5     
-   6     echo "--- To run the application, use the following command: ---"
-   7     echo "--- GEMINI_API_KEY=\"YOUR_API_KEY\" python3 main.py ---"
+echo "--- Installing system packages and Python dependencies ---"
+sudo apt-get update && sudo apt-get install -y python3-pip python3-tk libappindicator3-1
+pip3 install -r requirements.txt
+echo "--- To run the application, use the following command: ---"
+echo "--- GEMINI_API_KEY=\"AIzaSyA4KmRpgNMTv_-t88kbViC-u9opf2iJoeQ\" python3 main.py ---"
